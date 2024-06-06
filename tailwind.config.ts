1ed054afe019f568d3b715be5f9289356e315ce1
vandilsonbrito <vandilson25@outlook.com>
2024-06-06 16:09:41 -0300

    Create project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { url } from "inspector";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -9,11 +10,14 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero": "url('/hero.webp')",
       },
-    },
+      colors: {
+        primary: '#19464b',
+        secundary: '#3f5f5b',
+        greenAvocado: '#3f5f5b'
+      }
+    }
   },
   plugins: [],
 };
