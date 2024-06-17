3d2659873c3755437f8857e725c4977202366450
vandilsonbrito <vandilson25@outlook.com>
2024-06-17 18:07:46 -0300

    Feat: add reactquery provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import ReactQueryProvider from "@/utils/providers/ReactQueryProvider";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        {children}
-      </body>
+        <body className={montserrat.className}>
+            <ReactQueryProvider>  
+              <link rel="icon" href="/favicon.ico" sizes="any" />
+              {children}
+            </ReactQueryProvider>
+        </body>
     </html>
   );
 }
