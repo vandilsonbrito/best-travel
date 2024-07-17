b21ad63ca86d0fb0412bb3d8ea80be7748ebb1fb
vandilsonbrito <vandilson25@outlook.com>
2024-07-17 16:34:27 -0300

    Feat: update carousel and populate based on suggestionsPlaces  array

--- components/suggestions/Suggestions.tsx
+++ components/suggestions/Suggestions.tsx
@@ -1,205 +1,81 @@
 'use client'
 import Image from "next/image";
 import "./style.css"
-import { useEffect, useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 
 function Suggestions() {
 
-    const [imgNumber, setImgNumber] = useState(1);
-
-    const previousPicture = () => {
-        setImgNumber(imgNumber - 1);
-        if (imgNumber === 1) return setImgNumber(6);
-    
-    }
-    const nextPicture = () => {
-        setImgNumber(imgNumber + 1);
-        if (imgNumber > 5) return setImgNumber(1)
-    }
-
-
-    useEffect(() => {
-        const showImg = () => {
-            const imgsCollection = document.getElementsByClassName('principal-imgs');
-            const imgs = Array.from(imgsCollection);
-            imgs.map((item) => {
-                item.classList.add('hidden');
-            })
-            imgs[imgNumber - 1 ]?.classList.remove('hidden');
+    const suggestionsPlaces = [
+        {
+            img: '/el-chaten.jpg',
+            description: 'El Chatén, Argentina'
+        },
+        {
+            img: '/rio-de-janeiro.jpg',
+            description: 'Rio de Janeiro, Brasil'
+        },
+        {
+            img: '/cinque-terre.jpg',
+            description: 'Cinque Terre, Italy'
+        },
+        {
+            img: '/tulum.jpg',
+            description: 'Tulum, Mexico'
+        },
+        {
+            img: '/thailand.jpg',
+            description: 'Koh Phi Phi, Thailand'
+        },
+        {
+            img: '/switzerland.jpg',
+            description: 'Switzerland'
+        },
+        {
+            img: '/pragser-wildsee.jpg',
+            description: 'Pragser Wildsee, Italy'
+        },
+        {
+            img: '/capadoccia.jpg',
+            description: 'Capadoccia, Turkey'
+        },
         
-            const thumbnails = document.getElementsByClassName('thumbnails');
-            const imgsThumbnails = Array.from(thumbnails);
-            imgsThumbnails.map((item) => {
-                item.classList.add('opacity-40');
-            })
-            imgsThumbnails[imgNumber - 1 ]?.classList.remove('opacity-40');
-            imgsThumbnails[imgNumber - 1 ]?.classList.add('opacity-100');
-        }
-        showImg()
-    }, [imgNumber])
+    ]
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+      };
 
     return (
         <section className="w-full h-full ">
             <div className="row w-full h-full flex flex-col justify-center items-center bg-primary px5 md:px-20 pt-14 pb-20 ">
                 
-            <h2 className="text-lg lg:text-[1.5rem] text-white  lg:font-semibold mb-8 text-center tracking-wide">Favorite Destinations</h2>
-                <div className="w-[300px] h-[230px] md:w-[500px] md:h-[350px] lg:w-[700px] lg:h-[440px] overflow-hidden relative">
-                    <div className="w-full h-full principal-imgs" 
-                    >
-                        <Image
-                        src='/img1.jpg'
-                        width={700}
-                        height={0}
-                        alt=""
-                        quality={100}
-                        ></Image>
-                        <span className="absolute bottom-0 w-full p-2 bg-secundary text-center flex flex-col self-end text-white text-[.7rem] lg:text-base lg:font-semibold">Cinque Terre, Italy</span>
-                    </div>
-                    <div
-                        className="w-full h-full principal-imgs"
-                        onClick={() => {
-                        
-                        }}
-                    >
-                        <Image  
-                        src='/img2.jpg' 
-                        width={700}
-                        height={70}
-                        alt=""
-                        quality={100} />
-                        <span className="absolute bottom-0 w-full p-2 bg-secundary text-center flex flex-col self-end text-white text-[.7rem] lg:text-base lg:font-semibold">Rio de Janeiro, Brazil</span>
-                    </div>
-                    <div
-                        className="w-full h-full principal-imgs"
-                        onClick={() => {
-                            
-                        }}
-                    >
-                        <Image  
-                        src='/img3.jpg' 
-                        width={700}
-                        height={70}
-                        alt=""
-                        quality={100} />
-                        <span className="absolute bottom-0 w-full p-2 bg-secundary text-center flex flex-col self-end text-white text-[.7rem] lg:text-base lg:font-semibold">Arraial do Cabo, Rio de Janeiro, Bazil</span>
-                    </div>
-                    <div
-                        className="w-full h-full principal-imgs"
-                        onClick={() => {
-                        
-                        }}
-                    >
-                        <Image  
-                        src='/img4.jpg' 
-                        width={700}
-                        height={70}
-                        alt=""
-                        quality={100} 
-                        />
-                        <span className="absolute bottom-0 w-full p-2 bg-secundary text-center flex flex-col self-end text-white text-[.7rem] lg:text-base lg:font-semibold">Cappadocia, Turkey</span>
-                    </div>
-                    <div
-                        className="w-full h-full principal-imgs"
-                        onClick={() => {
-                        
-                        }}
-                    >
-                        <Image  
-                        src='/img5.jpg' 
-                        width={700}
-                        height={70}
-                        alt=""
-                        quality={100} 
-                        />
-                        <span className="absolute bottom-0 w-full p-2 bg-secundary text-center flex flex-col self-end text-white text-[.7rem] lg:text-base lg:font-semibold">Phi Phi Islands, Thailand</span>
-                    </div>
-                    <div
-                        className="w-full h-full principal-imgs"
-                        onClick={() => {
-                        
-                        }}
-                    >
-                        <Image  
-                        src='/img6.jpg' 
-                        width={700}
-                        height={70}
-                        alt=""
-                        quality={100} 
-                        />
-                        <span className="absolute bottom-0 w-full p-2 bg-secundary text-center flex flex-col self-end text-white text-[.7rem] lg:text-base lg:font-semibold">Furka Pass, Obergoms, Switzerland</span>
-                    </div>
-
-                    <a className="prev" onClick={previousPicture}>❮</a>
-                    <a className="next" onClick={nextPicture}>❯</a>
+                <h2 className="text-lg lg:text-[1.5rem] text-white  lg:font-semibold mb-8 text-center tracking-wide">Favorite Destinations</h2>
+                <div className="w-full h-full">
+                    <Slider {...settings}>
+                    {
+                        suggestionsPlaces.map((places, index) => (
+                            <div key={index} className="px-3">
+                                <Image 
+                                src={places.img}
+                                alt={places.description}
+                                width={400}
+                                height={700}
+                                className="h-[500px] rounded-lg"
+                                />
+                                <p className="text-white text-center mt-2">{places.description}</p>
+                            </div>
+                        ))
+                    }   
+                    </Slider>
                 </div>
-                
-
-                <div className="w-[300px] md:w-[500px] lg:w-[700px] h-fit flex justify-center items-center ">
-                    <div className="w-full h-[60px] flex">
-                        <div
-                            className="thumbnails"
-                        >
-                            <Image
-                            src='/img1.jpg'
-                            width={150}
-                            height={70}
-                            onClick={() => setImgNumber(1)}
-                            className="aspect-video hover-shadow cursor-pointer"
-                            alt="Image Itália"
-                            />
-                        </div>
-                        <div className="thumbnails">
-                            <Image
-                            src='/img2.jpg'
-                            width={150}
-                            height={70}
-                            onClick={() => setImgNumber(2)}
-                            className="aspect-video hover-shadow cursor-pointer"
-                            alt="Image of Rio de Janeiro"
-                            />
-                        </div>
-                        <div className="thumbnails">
-                            <Image
-                            src='/img3.jpg'
-                            width={150}
-                            height={70}
-                            onClick={() => setImgNumber(3)}
-                            className="aspect-video hover-shadow cursor-pointer"
-                            alt="Image Arraial do Cabo, Brazil"
-                            />
-                        </div>
-                        <div className=" thumbnails">
-                            <Image
-                            src='/img4.jpg'
-                            width={150}
-                            height={70}
-                            onClick={() => setImgNumber(4)}
-                            className="aspect-video hover-shadow cursor-pointer"
-                            alt="Image of Rio de Janeiro"
-                            />
-                        </div>
-                        <div className=" thumbnails">
-                            <Image
-                            src='/img5.jpg'
-                            width={150}
-                            height={70}
-                            onClick={() => setImgNumber(5)}
-                            className="aspect-video hover-shadow cursor-pointer"
-                            alt="Image of Rio de Janeiro"
-                            />
-                        </div>
-                        <div className=" thumbnails">
-                            <Image
-                            src='/img6.jpg'
-                            width={150}
-                            height={70}
-                            onClick={() => setImgNumber(6)}
-                            className="aspect-video hover-shadow cursor-pointer"
-                            alt="Image of Rio de Janeiro"
-                            />
-                        </div>
-                    </div>
-                </div>
+           
+                         
             </div>
         </section>
     )
