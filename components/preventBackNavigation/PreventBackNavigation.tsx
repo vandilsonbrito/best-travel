27402163864e782be321805928184d5db40989c5
vandilsonbrito <vandilson25@outlook.com>
2024-07-26 22:44:40 -0300

    Create prevent back navigation component

---
+++ components/preventBackNavigation/PreventBackNavigation.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const PreventBackNavigation = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handlePopState = (event: PopStateEvent) => {
+      router.push('/search-flights');
+    };
+
+    window.history.pushState(null, '', window.location.href);
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [router]);
+
+  return null;
+};
+
+export default PreventBackNavigation;
