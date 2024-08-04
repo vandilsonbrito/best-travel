6fabecfe1754f131a5a53bd82b9c74d863a2bb33
vandilsonbrito <vandilson25@outlook.com>
2024-08-03 23:04:54 -0300

    Set sessionStorage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+'use client'
 import Banner from '../components/banner/Banner';
 import Suggestions from '../components/suggestions/Suggestions';
 import Footer from '../components/footer/Footer';
+import { useEffect } from 'react';
 
 
 export default function Home() {
+
+  useEffect(() => {
+    sessionStorage.setItem('isEmailSent', '');
+  }, []); 
+  
   return (
     <main className="w-full flex min-h-screen flex-col items-center justify-between "> 
       { <Banner/> && <Suggestions/> && <Footer/> ? 
