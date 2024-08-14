0683aae7e8eb9553212f5a7cbaa4ac3d3793a516
vandilsonbrito <vandilson25@outlook.com>
2024-08-14 19:04:33 -0300

    fix: adds preventBackNavigation component on page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,7 @@ import Banner from '../../components/banner/Banner';
 import Suggestions from '../../components/suggestions/Suggestions';
 import Footer from '../../components/footer/Footer';
 import { useEffect } from 'react';
+import PreventBackNavigation from '../../components/preventBackNavigation/PreventBackNavigation';
 
 
 export default function Home() {
@@ -12,19 +13,22 @@ export default function Home() {
   }, []); 
   
   return (
-    <main className="w-full flex min-h-screen flex-col items-center justify-between "> 
-      { <Banner/> && <Suggestions/> && <Footer/> ? 
-        <>
-          <Banner/>
-          <Suggestions/>
-          <Footer/>
-        </>
-        :
-        <div className="w-full h-full min-h-screen flex flex-col justify-center items-center">
-          <span className="loader"></span>
-        </div>
-      }
-        
-    </main>
+    <>
+      <PreventBackNavigation/>
+      <main className="w-full flex min-h-screen flex-col items-center justify-between ">
+        { <Banner/> && <Suggestions/> && <Footer/> ?
+          <>
+            <Banner/>
+            <Suggestions/>
+            <Footer/>
+          </>
+          :
+          <div className="w-full h-full min-h-screen flex flex-col justify-center items-center">
+            <span className="loader"></span>
+          </div>
+        }
+      
+      </main>
+    </>
   );
 }
